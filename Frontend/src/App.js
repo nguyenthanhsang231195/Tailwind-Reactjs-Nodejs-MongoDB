1cc06e7dc850ddc81d72ca5b348cb0202bff9ec6
unknown <you@example.com>
2021-11-03 13:00:59 +0700

    Frontend-Start

---
+++ Frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+<div className="App">
+    <header className="header-website">
+        Header
+    </header> 
+
+    <main className="main-website">
+        Main
+    </main>
+
+    <footer className="footer-website">
+        Designed by Sang 2021
+    </footer>
+</div>
+    );
+}
+
+export default App;
---
+++ Frontend/src/Data/policy.js
@@ -0,0 +1,24 @@
+const policy = [
+    {
+        name: "Miễn phí giao hàng",
+        description: "Miễn phí ship với hóa đơn > 399K",
+        icon: "fas fa-money-bill-wave"
+    },
+    {
+        name: "Thanh toán COD",
+        description: "Thanh toán khi nhận hàng",
+        icon: "fas fa-gem"
+    },
+    {
+        name: "Khách hàng VIP",
+        description: "Tặng quà/Phiếu giảm giá tháng",
+        icon: "fas fa-people-carry"
+    },
+    {
+        name: "Hỗ trợ bảo hành",
+        description: "Đổi, sửa đồ tại tất cả cửa hàng",
+        icon: "fas fa-wrench"
+    }
+]
+
+export default policy;
